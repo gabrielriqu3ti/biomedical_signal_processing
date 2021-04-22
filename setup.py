ef66aad720c90c1547854eec4505795abfca8bb7
gabrielriqu3ti <gabrielhriqueti@gmail.com>
2021-04-21 21:16:30 -0300

    Add installation information

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ Created on Tue Apr 20 14:59:39 2021
 @author: Gabriel H Riqueti
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='Biomedical_Signal_Processing')
+setup(
+      name="Biomedical_Signal_Processing",
+      version="0.0.1",
+      author="Gabriel H. Riqueti",
+      author_email="gabrielhriqueti@gmail.com",
+      packages=find_packages(include=['Nernst Equation']),
+)
